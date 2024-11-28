83cd6d7618b987fa9238dc632e2a3263b2322937
ayouzou <ayoubzouiine@gmail.com>
2024-11-28 14:55:47 +0100

    added auth with google and github to the project

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LoginForm from '../components/LoginForm'
+import AuthButtons from '../components/AuthButtons'
 
 const page = () => {
     return (
-        <div>
+        <main className="flex flex-col justify-center items-center my-auto ">
             <LoginForm />
-        </div>
+            <AuthButtons/>
+        </main>
     )
 }
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import RegisterForm from '../components/RegisterForm'
+import AuthButtons from '../components/AuthButtons'
 
 const page = () => {
     return (
-        <main>
+        <main className="flex flex-col justify-center items-center my-auto ">
             <RegisterForm />
+            <AuthButtons/>
         </main>
     )
 }
